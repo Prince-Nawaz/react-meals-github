e91a9420fd8a8f5727c782c3abb172772871f241
P S K Abdul Nawaz <princenawaz007@gmail.com>
2022-02-04 08:22:56 +0530

    React Meals initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <CartProvider>
+        <Header />
+        <main>
+          <Meals />
+        </main>
+      </CartProvider>
+      );
     </div>
   );
 }
